217160af01eebd7233f01b2d7bcf66033536d58c
rgero <rgero@student.21-school.ru>
2019-10-14 18:55:17 +0300

    1

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: rgero <rgero@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/14 16:16:01 by rgero             #+#    #+#             */
-/*   Updated: 2019/10/14 16:27:50 by rgero            ###   ########.fr       */
+/*   Updated: 2019/10/14 18:50:48 by rgero            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,21 @@
 
 typedef struct		s_link
 {
-	char			letter;
-	unsigned int	tetriminos;
+	unsigned char	letter;
+	unsigned char	bit;
+	unsigned char	header;
 	struct s_link	*left;	
 	struct s_link	*right;	
 	struct s_link	*up;	
-	struct s_link	*down;	
+	struct s_link	*down;
+	struct s_link	*root;	
 }					t_link;
+
+typedef struct		s_stack
+{
+	struct s_link	*link;
+	struct s_stack	*next;
+}					t_stack;
 
 
 #endif
