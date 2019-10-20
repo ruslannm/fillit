045eb00996e44434639cd3148602856cf7a6e4a4
rgero <rgero@student.21-school.ru>
2019-10-20 17:21:25 +0300

    del *.o

--- ft_dancing_links.c
+++ ft_dancing_links.c
@@ -6,7 +6,7 @@
 /*   By: rgero <rgero@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/20 16:28:21 by rgero             #+#    #+#             */
-/*   Updated: 2019/10/20 17:01:05 by rgero            ###   ########.fr       */
+/*   Updated: 2019/10/20 17:17:53 by rgero            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,16 @@ static int  ft_square(int nb)
 {
     while (0 == ft_sqrt(nb))
         nb++;
-    return (nb);
+    return (ft_sqrt(nb));
 }
 
 char    *ft_dancing_links(t_list *income, int qnt)
 {
     t_list  *tmp;
-    int     i;
+    int     len;
 
     tmp = income;
-    i = ft_square(qnt * 4);
+    len = ft_square(qnt * 4);
+    
     return("ok\n");
 }
