6bdc39927434edba8b8477d3c56efead5cd47bde
rgero <rgero@student.21-school.ru>
2019-10-23 18:11:25 +0300

    1

--- ft_dancing_links.c
+++ ft_dancing_links.c
@@ -6,7 +6,7 @@
 /*   By: rgero <rgero@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/20 16:28:21 by rgero             #+#    #+#             */
-/*   Updated: 2019/10/20 17:17:53 by rgero            ###   ########.fr       */
+/*   Updated: 2019/10/23 18:10:31 by rgero            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,53 @@
 
 static int  ft_square(int nb)
 {
-    while (0 == ft_sqrt(nb))
-        nb++;
-    return (ft_sqrt(nb));
+	while (0 == ft_sqrt(nb))
+		nb++;
+	return (ft_sqrt(nb));
 }
 
 char    *ft_dancing_links(t_list *income, int qnt)
 {
-    t_list  *tmp;
-    int     len;
+	t_list  *tmp;
+	int     len;
 
-    tmp = income;
-    len = ft_square(qnt * 4);
-    
-    return("ok\n");
+	tmp = income;
+	len = ft_square(qnt * 4);
+	
+	return("ok\n");
+}
+
+int		ft_count_row(t_link *header)
+{
+	t_link	*tmp;
+	int		i;
+
+	tmp = header;
+	i = 0;
+	while (tmp->down != header)
+	{
+		i++;
+		tmp = tmp->down;
+	}
+	return (i);
+}
+
+t_link *ft_get_header_min(t_link *root)
+{
+	t_link	*tmp;
+	t_link	*ret;
+	int		amount_row;
+	int		i;
+
+	i = 0;
+	ret = NULL;
+	amount_row = ft_count_row(root);
+	tmp = root;
+	while ((tmp = tmp->right) != root)
+	{
+		i = ft_count_row(tmp);
+		if (i < amount_row)
+			ret = tmp;
+	}
+	return (ret);
 }
