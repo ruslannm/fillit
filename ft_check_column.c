db5a9974450e0d749a82c9b13ebc77b608df4f9e
rgero <rgero@student.21-school.ru>
2019-10-24 19:43:35 +0300

    add check column

--- ft_check_column.c
+++ ft_check_column.c
@@ -6,7 +6,7 @@
 /*   By: rgero <rgero@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/24 19:31:05 by rgero             #+#    #+#             */
-/*   Updated: 2019/10/24 19:38:42 by rgero            ###   ########.fr       */
+/*   Updated: 2019/10/24 19:43:03 by rgero            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 /* 0 - not column (solution)
    1 - there are column (go ahead)
-  -1 - there are empty column (go back, undo deletion)
+   -1 - there are empty column (go back, undo deletion)
 */ 
 
 int ft_check_column(t_link *root)
@@ -22,11 +22,15 @@ int ft_check_column(t_link *root)
     t_link  *tmp;
     int ret;
 
-    ret = 1;
+    ret = 0;
     tmp = root;
     while (tmp->right != root)
     {
         tmp = tmp->right;
-        ft_count_row(tmp, "all");
+        if (ft_count_row(tmp, "all") == 0)
+            return (-1);
+        else
+            ret = 1;        
     }
+    return (ret);
 }
