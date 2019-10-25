57cfa90b824e1725a96e85abc64eb6f7a4e8dd78
rgero <rgero@student.21-school.ru>
2019-10-25 17:35:05 +0300

    refact

--- ft_dancing_links.c
+++ ft_dancing_links.c
@@ -6,7 +6,7 @@
 /*   By: rgero <rgero@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/20 16:28:21 by rgero             #+#    #+#             */
-/*   Updated: 2019/10/25 16:43:43 by rgero            ###   ########.fr       */
+/*   Updated: 2019/10/25 17:34:50 by rgero            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,41 +19,39 @@ static int  ft_square(int nb)
 	return (ft_sqrt(nb));
 }
 
-int    ft_dancing_links(t_link *root, t_list **solution)
-{
-	int		ret;
-	t_link	*row;
-	t_link	*tmp_row_delete;
-	t_link	*tmp_column;
-	t_list	*stack_delete;
-	t_link	*root_top;
 	//tmp = income;
 	//len = ft_square(qnt * 4);
 	
 //	row = (ft_get_header_min(root))->down; //first row for solution
 //	tmp_row = row;
-	ret = 0;
-	if ((root_top = root->right) == root)
-		return (1);  //there aren't any column
 
+int    ft_dancing_links(t_link *root, t_list **solution)
+{
+	int		ret;
+	t_link	*row;
+	t_list	*stack_delete_row;
+	t_list	*stack_delete_top;
+	t_link	*root_top;
+	
+	ret = 0;
+	root_top = root->right;
+	row = root_top->down;  //first row
 	while (root_top != root)
 	{
-		row = root_top->down;  //first row
-		ft_move_same_letter(row, &stack_delete);   //delete row with same letter
-		ft_move_same_bits(row, &stack_delete);
+		ft_move_same_letter(row, &stack_delete_row);   //delete row with same letter
+		ft_move_same_bits(row, &stack_delete_row, &stack_delete_top);
 		ft_delete_dl(row, &(*solution), "row"); //move_to_solution
-	    if ((ret = ft_check_column(root)) == 0)
+	    ret = ft_check_column(root);
+		if (ret == 0)
 			return (1);
 		else if (ret == -1)
-				;	//undo deletion
+		{
+			ft_undo_move(&stack_delete_row, &stack_delete_top, *solution); 	//undo deletion
+			row = row->down; 		
+		}
 		else
-			root_top = root_top->right;
+			return (ft_dancing_links(root, &(*solution)));
 	}
-	if (root->right == root)
-		return (1);
-	else
-		return (ft_dancing_links(root, &(*solution)))
-	;
 }
 
 int		ft_count_row(t_link *top, char *mask)
@@ -94,49 +92,4 @@ t_link *ft_get_header_min(t_link *root)
 	if (!ret && root->right != root)
 		ret = root->right;  //work when tetra is missing
 	return (ret);
-}
-
-void	ft_delete_dl(t_link *link, t_list **stack, char *type)
-{
-	t_link *tmp;
-
-	tmp = link;
-	if (type == "row" && tmp->root_side->letter != 0) //defence deleting header
-	{	
-		while (tmp != link)
-		{
-			tmp->down->up = tmp->up;
-			tmp->up->down = tmp->down;
-			tmp = tmp->right;
-		}
-	}
-	else
-	{
-		tmp->right->left = tmp->left;
-		tmp->left->right = tmp->right;
-	}
-	ft_push(*stack, link);
-}
-
-void	ft_restore_dl(t_list **stack, char *type)
-{
-	t_link	*tmp;
-	t_link	*new;
-	
-	new = ft_pop(*stack);
-	tmp = new;
-	if (type == "row")
-	{
-		while (tmp != new)
-		{
-			tmp->down->up = tmp;
-			tmp->up->down = tmp;
-			tmp = tmp->right;
-		}
-	}
-	else
-	{
-		tmp->right->left = tmp;
-		tmp->left->right = tmp;
-	}
 }
--- ft_move_same_bits.c
+++ ft_move_same_bits.c
@@ -6,13 +6,13 @@
 /*   By: rgero <rgero@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/25 15:29:30 by rgero             #+#    #+#             */
-/*   Updated: 2019/10/25 16:13:20 by rgero            ###   ########.fr       */
+/*   Updated: 2019/10/25 17:12:39 by rgero            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-static void	ft_move_same_bit(t_link *row, t_link **stack)
+static void	ft_move_same_bit(t_link *row, t_link **stack_row, t_link **stack_top)
 {
 	t_link  *root_top;
 	t_link  *row_to_stack;
@@ -23,14 +23,14 @@ static void	ft_move_same_bit(t_link *row, t_link **stack)
 	{
 		row_to_stack = row_to_stack->down;
 		if (row_to_stack == row->root_top)
-			ft_delete_dl(row_to_stack, *stack, "column");
+			ft_delete_dl(row_to_stack, *stack_row, "column");
 		else
-			ft_delete_dl(row_to_stack, *stack, "row");
+			ft_delete_dl(row_to_stack, *stack_top, "row");
 		row_to_stack = row_to_stack->down;
 	}
 }
 
-void		ft_move_same_bits(t_link *row, t_link **stack)
+void		ft_move_same_bits(t_link *row, t_link **stack_row, t_link **stack_top)
 {
 	t_link  *tmp;
 	t_link  *root_side;
@@ -40,7 +40,7 @@ void		ft_move_same_bits(t_link *row, t_link **stack)
 	while (tmp->right != row)
 	{
 		if (root_side != tmp->root_top)
-			ft_move_same_bit(tmp, &(*stack));
+			ft_move_same_bit(tmp, &(*stack_row), &(*stack_top));
 		tmp = tmp->right;
 	}
 }
