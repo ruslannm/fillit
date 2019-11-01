9c911239939379dd3270ebf877ec7d0c8faa6987
rgero <rgero@student.21-school.ru>
2019-11-01 18:29:10 +0300

    change dl

--- ft_dancing_links.c
+++ ft_dancing_links.c
@@ -6,7 +6,7 @@
 /*   By: rgero <rgero@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/20 16:28:21 by rgero             #+#    #+#             */
-/*   Updated: 2019/11/01 16:45:03 by rgero            ###   ########.fr       */
+/*   Updated: 2019/11/01 18:11:18 by rgero            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -110,11 +110,16 @@ int	ft_dancing_links(t_link *root, t_link *row, t_stack **solution, int qnt)
 	int		check_s;
 	t_stack	*stack_delete_row;
 	t_stack	*stack_delete_top;
+	t_link	*tmp;
 
 	stack_delete_row = NULL;
 	stack_delete_top = NULL;
+	tmp = row;
 	while (1)
 	{
+		printf(" %p\n", row->root_side);
+		if (row->root_side->letter == 3)
+			printf("letter = 3\n");
 		ft_move_same_letter(row->root_side, &stack_delete_row);
 		ft_move_same_bits(row->root_side, &stack_delete_row, &stack_delete_top);
 		ft_delete_dl(row->root_side, &(*solution), 'r');
@@ -128,21 +133,26 @@ int	ft_dancing_links(t_link *root, t_link *row, t_stack **solution, int qnt)
 			ft_undo_move(&stack_delete_row, &stack_delete_top, &(*solution));
 			printf("After undo, ret=%d, check_s=%d=\n", ret, check_s);
 			ft_print_matrix(root);
-			if (row->root_side->down->letter > 27)
+			if (row->root_side->down->letter > 26)
 				return (-1);
+			row = row->root_side->down;
 		}
 		else if (check_s == qnt)
 		{
-			if ((ft_dancing_links(root, root->down, &(*solution), qnt)) == -1)
+			if ((ft_dancing_links(root, root->down, &(*solution), qnt)) == 1)
 			{
-				ft_undo_move(&stack_delete_row, &stack_delete_top, &(*solution));
-				printf("After recursive undo, ret=%d, check_s=%d=\n", ret, check_s);
-				ft_print_matrix(root);
+//				ret = ft_check_column(root);
+//				check_s = ft_check_solution(*solution, qnt) + ft_check_root(root, qnt);
+//				if (ret == 1 && check_s == qnt)
+					return (1);
 			}
-			if (row->root_side->down == root)
+			ft_undo_move(&stack_delete_row, &stack_delete_top, &(*solution));
+			printf("After recursive undo, ret=%d, check_s=%d=\n", ret, check_s);
+			ft_print_matrix(root);
+			if (tmp->root_side->down == root)
 				return (-1);
+			row = tmp->root_side->down;
 		}
-		row = row->root_side->down;
 	}
 	return (-1);
 }
