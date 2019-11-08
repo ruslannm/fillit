6a6a6530fad773f6053d590c7f1b8582716ef4de
Frenica Provolone <fprovolo@ox-f2.21-school.ru>
2019-11-08 18:59:50 +0300

    1

--- Makefile
+++ Makefile
@@ -6,29 +6,29 @@
 #    By: fprovolo <fprovolo@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/09/13 15:46:56 by rgero             #+#    #+#              #
-#    Updated: 2019/11/06 18:17:52 by fprovolo         ###   ########.fr        #
+#    Updated: 2019/11/08 18:54:21 by fprovolo         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = fillit
 
 CC = gcc
-CFLAGS = -Wall -Wextra -Werror -g
+CFLAGS = -Wall -Wextra -Werror
 
 SRC_PATH = ./
 SRC_NAME = main.c ft_move_tetra.c ft_sqrt.c ft_fill_matrix.c \
 	ft_dancing_links.c	ft_stack_pop.c ft_stack_push.c \
-	ft_move_same_bits.c ft_move_same_letter.c ft_print_matrix.c \
+	ft_move_same_bits.c ft_move_same_letter.c \
 	ft_create_blank_line.c ft_add_dummy.c ft_delete_dl.c \
 	ft_strnew_char.c ft_size_tetra.c ft_init_header.c \
-	ft_check_links.c \
+	ft_check_links.c ft_lstaddback.c \
 	ft_row_for_search.c ft_delete_root.c ft_del_content.c
 
 OBJ_PATH = ./
 OBJ_NAME = $(SRC_NAME:.c=.o)
 OBJ = $(addprefix $(OBJ_PATH), $(OBJ_NAME))
 
-INC_PATH = ./
+INC_PATH = ./libft/
 INC = $(addprefix -I, $(INC_PATH))
 
 LIB_PATH = libft/
@@ -38,13 +38,13 @@ LIB_NAME = libft.a
 
 all: $(NAME)
 
-$(NAME): $(LIB_NAME) $(OBJ_NAME)
+$(NAME): $(LIB_PATH)$(LIB_NAME) $(OBJ_NAME)
 	$(CC) -o $(NAME)  $(OBJ_NAME) -L $(LIB_PATH) -lft
 
-%.o: %.c
-	$(CC) $(CFLAGS) -o $@  -c $<
+%.o: %.c $(NAME).h
+	$(CC) $(CFLAGS) -I $(INC_PATH) -o $@  -c $<
 
-$(LIB_NAME):
+$(LIB_PATH)$(LIB_NAME):
 	make -C $(LIB_PATH)
 
 clean:
--- fillit.h
+++ fillit.h
@@ -6,24 +6,24 @@
 /*   By: fprovolo <fprovolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/14 16:16:01 by rgero             #+#    #+#             */
-/*   Updated: 2019/11/06 18:16:48 by fprovolo         ###   ########.fr       */
+/*   Updated: 2019/11/08 18:54:35 by fprovolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLIT_H
 # define FILLIT_H
 
-#include <stdio.h>
-#include "libft/get_next_line.h"
-#include "libft/libft.h"
+# include "get_next_line.h"
+# include "libft.h"
+
 
 typedef struct		s_link
 {
 	unsigned char	letter;
 	unsigned char	bit;
-	struct s_link	*left;	
-	struct s_link	*right;	
-	struct s_link	*up;	
+	struct s_link	*left;
+	struct s_link	*right;
+	struct s_link	*up;
 	struct s_link	*down;
 	struct s_link	*root_top;
 	struct s_link	*root_side;
@@ -35,38 +35,41 @@ typedef struct		s_stack
 	struct s_stack	*next;
 }					t_stack;
 
-char	*ft_move_tetra(char *s, int len);
-int		ft_check_tetra(char *s, t_list **income);
-char	*ft_strnew_char(size_t size, char c);
-int		ft_dancing_links(t_link *root, t_stack *stack_row, t_stack **solution, int qnt);
-int		ft_dancing_links_fast(t_link *root, t_stack *stack_row, t_stack **solution,
-		int qnt);
-int 	ft_sqrt(int nb);
-void    ft_print_field(t_link *ptr, int size);
-int     ft_check_fit(char *tet, int pt, int size);
-t_link	*ft_create_blank_line(int len, t_stack **matrix_stk);
-void	ft_free_line(t_link *line);
-void	ft_free_matrix(t_link *root);
-t_link	*ft_fill_matrix(t_list *income, int size, t_stack **matrix_stk, char dummy);
-t_link	*ft_init_header(int size, t_stack **matrix_stk);
-void	ft_add_to_matrix(t_link *root, t_link *line);
-void	ft_add_tetra(t_link *root, char *tet, int pt, t_link *new);
-int		ft_add_dummy(t_link *root, t_stack **matrix_stk);
-void    ft_print_matrix(t_link *root);
-void	ft_delete_dl(t_link *link, t_stack **stack, char type);
-void    ft_undo_move(t_stack **stack_row, t_stack **stack_top, t_stack **solution);
-void    ft_move_same_letter(t_link *row, t_stack **stack);
-void	ft_move_same_bits(t_link *row, t_stack **stack_row, t_stack **stack_top);
-int		ft_push(t_stack **stack, t_link *link);
-t_link	*ft_pop(t_stack **stack);
-void	ft_del_stack(t_stack *stack);
-t_stack	*ft_row_for_seach(t_link *root);
-t_stack	*ft_row_for_seach_fast(t_link *root);
-int     ft_size_tetra(char *tetra);
-int		ft_check_solution(t_stack *solution, int qnt);
-int		ft_check_root(t_link *root, int qnt);
-int		ft_count_row(t_link *top);
-void	ft_del_root(t_stack *stack);
-void	ft_del(void *content, size_t len);
+char				*ft_move_tetra(char *s, int len);
+int					ft_check_tetra(char *s, t_list **income);
+char				*ft_strnew_char(size_t size, char c);
+int					ft_dancing_links(t_link *root, t_stack *stack_row,
+					t_stack **solution, int qnt);
+int					ft_dancing_links_fast(t_link *root, t_stack *stack_row,
+					t_stack **solution, int qnt);
+int					ft_sqrt(int nb);
+int					ft_check_fit(char *tet, int pt, int size);
+t_link				*ft_create_blank_line(int len, t_stack **matrix_stk);
+void				ft_free_line(t_link *line);
+void				ft_free_matrix(t_link *root);
+t_link				*ft_fill_matrix(t_list *income, int size,
+					t_stack **matrix_stk, char dummy);
+t_link				*ft_init_header(int size, t_stack **matrix_stk);
+void				ft_add_to_matrix(t_link *root, t_link *line);
+void				ft_add_tetra(t_link *root, char *tet, int pt, t_link *new);
+int					ft_add_dummy(t_link *root, t_stack **matrix_stk);
+void				ft_delete_dl(t_link *link, t_stack **stack, char type);
+void				ft_undo_move(t_stack **stack_row, t_stack **stack_top,
+					t_stack **solution);
+void				ft_move_same_letter(t_link *row, t_stack **stack);
+void				ft_move_same_bits(t_link *row, t_stack **stack_row,
+					t_stack **stack_top);
+int					ft_push(t_stack **stack, t_link *link);
+t_link				*ft_pop(t_stack **stack);
+void				ft_del_stack(t_stack *stack);
+t_stack				*ft_row_for_seach(t_link *root);
+t_stack				*ft_row_for_seach_fast(t_link *root);
+int					ft_size_tetra(char *tetra);
+int					ft_check_solution(t_stack *solution, int qnt);
+int					ft_check_root(t_link *root, int qnt);
+int					ft_count_row(t_link *top);
+void				ft_del_root(t_stack *solution, t_stack *root);
+void				ft_del(void *content, size_t len);
+void				ft_lstaddback(t_list **alst, t_list *new);
 
 #endif
